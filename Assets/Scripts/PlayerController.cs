c04c36f3f19a484d04f880ef9144376a1fbeb929
Daniel Muckelbauer <daniel2.muckelbauer@oth-regensburg.de>
2018-09-17 22:00:54 +0200

    Created first character, ground and superfast movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private float moveForce = 365f;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        rigidBody.AddForce(Vector2.right * horizontal * moveForce);
+    }
+}
