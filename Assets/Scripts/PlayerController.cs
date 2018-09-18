9f5eafd981bacb4c35a1429b544fb378bfb4da16
Daniel Muckelbauer <daniel2.muckelbauer@oth-regensburg.de>
2018-09-18 13:22:21 +0200

    Added flip and MaxSpeed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,33 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public float MoveForce;
+
+    private const float MaxSpeed = 10;
     private Rigidbody2D rigidBody;
-    private float moveForce = 365f;
+    private SpriteRenderer spriteRenderer;
+    private bool lookingRight = true;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        rigidBody.AddForce(Vector2.right * horizontal * moveForce);
+        if (lookingRight && horizontal < 0)
+        {
+            lookingRight = false;
+            spriteRenderer.flipX = true;
+        }
+        else if (!lookingRight && horizontal > 0)
+        {
+            lookingRight = true;
+            spriteRenderer.flipX = false;
+        }
+        if (Mathf.Abs(horizontal) < MaxSpeed)
+            rigidBody.AddForce(Vector2.right * horizontal * MoveForce);
     }
 }
