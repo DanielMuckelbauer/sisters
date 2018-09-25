4a8d6537f4053af9416d0a14608a51404c3e3378
Daniel Muckelbauer <daniel2.muckelbauer@oth-regensburg.de>
2018-09-25 20:29:28 +0200

    PlayerCameraController

---
+++ Assets/Code/Classes/PlayerCameraController.cs
@@ -0,0 +1,21 @@
+﻿using Code.Scripts;
+using UnityEngine;
+
+namespace Code.Classes
+{
+    public class PlayerCameraController
+    {
+        private readonly Transform camera;
+
+        public PlayerCameraController(Transform camera)
+        {
+            this.camera = camera;
+        }
+
+        public void FixOn(Transform player)
+        {
+            camera.transform.position = new Vector3(player.position.x, player.position.y,
+                camera.transform.position.z);
+        }
+    }
+}
---
+++ Assets/Code/Scripts/PlayerCamera.cs
@@ -0,0 +1,22 @@
+﻿using Code.Classes;
+using UnityEngine;
+
+namespace Code.Scripts
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        public Transform Player;
+
+        private PlayerCameraController playerCameraController;
+
+        private void Start()
+        {
+            playerCameraController = new PlayerCameraController(gameObject.transform);
+        }
+
+        private void FixedUpdate()
+        {
+            playerCameraController.FixOn(Player);
+        }
+    }
+}
