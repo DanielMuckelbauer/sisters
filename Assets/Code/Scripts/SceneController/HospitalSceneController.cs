3415b5f5b7722a0980b97b96b72272be9ad9ab67
Daniel Muckelbauer <dadamuck@hotmail.com>
2018-10-14 18:25:39 +0200

    Title screen and intro times

--- Assets/Code/Scripts/SceneController/HospitalSceneController.cs
+++ Assets/Code/Scripts/SceneController/HospitalSceneController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 namespace Code.Scripts.SceneController
@@ -20,11 +21,20 @@ namespace Code.Scripts.SceneController
         public Vector3 BirthPosition;
         public SpriteRenderer Stars;
         public Sprite SwordRoom;
+        public SpriteRenderer Title;
         public Animator CameraAnimator;
         public Animator DaniAnimator;
+        public Transform Sword;
+        public Transform SwordTarget;
 
         private List<GameObject> birthedObjects;
         private const float BirthForce = 250;
+        private bool moveSword;
+
+        private void Update()
+        {
+            MoveSwordDown();
+        }
 
         private void Start()
         {
@@ -78,26 +88,45 @@ namespace Code.Scripts.SceneController
         private IEnumerator PlayCutScene()
         {
             InitializeCutsceneStrings();
-            yield return ShowNextTextSection(1);
-            yield return ShowNextTextSection(1);
+            yield return ShowNextTextSection(5);
+            yield return ShowNextTextSection(5);
             yield return HospitalCutscene();
-            ReactivateTextAndPlayIntroMusic();
-            yield return ShowNextTextSection(1);
+            AudioPlayer.Stop();
+            ReactivateText();
+            yield return ShowNextTextSection(5);
+            PlayIntroMusic();
             yield return SwordRoomCutscene();
             yield return BubbleSpeek();
             DaniAnimator.SetTrigger("GiveSword");
             yield return new WaitForSeconds(5);
             CameraAnimator.SetTrigger("MoveCameraUp");
+            yield return new WaitForSeconds(5);
+            StartCoroutine(MakeTitleAppear());
+            moveSword = true;
         }
 
-        private void ReactivateTextAndPlayIntroMusic()
+        private void MoveSwordDown()
+        {
+            if (!moveSword)
+                return;
+            const float speed = 4.5f;
+            if (Sword.position.y <= SwordTarget.position.y)
+                moveSword = false;
+            float step = speed * Time.deltaTime;
+            Sword.position = Vector3.MoveTowards(Sword.position, SwordTarget.position, step);
+        }
+
+        private void ReactivateText()
         {
             birthedObjects.ForEach(Destroy);
-            AudioPlayer.Stop();
-            AudioPlayer.clip = AudioClips[0];
-            AudioPlayer.Play();
             Background.GetComponent<SpriteRenderer>().enabled = false;
             UiCanvas.SetActive(true);
+        }
+
+        private void PlayIntroMusic()
+        {
+            AudioPlayer.clip = AudioClips[0];
+            AudioPlayer.Play();
         }
 
         private void SetUpSpeechBubble()
@@ -113,7 +142,7 @@ namespace Code.Scripts.SceneController
             Stars.enabled = true;
             Dani.SetActive(true);
             Girls.SetActive(true);
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(3);
         }
 
         private IEnumerator ThrowObject(GameObject objectToThrow)
@@ -124,6 +153,19 @@ namespace Code.Scripts.SceneController
             rigidBody.AddForce((Vector2.up + Vector2.left) * BirthForce);
             rigidBody.AddTorque(2, ForceMode2D.Impulse);
             yield return new WaitForSeconds(1.5f);
+        }
+
+        private IEnumerator MakeTitleAppear()
+        {
+            const float duration = 5.0f;
+            float t = 0;
+            float startTime = Time.time;
+            while (t < 1)
+            {
+                t = (Time.time - startTime) / duration;
+                Title.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(0, 1, t));
+                yield return new WaitForSeconds(0.2f);
+            }
         }
     }
 }
