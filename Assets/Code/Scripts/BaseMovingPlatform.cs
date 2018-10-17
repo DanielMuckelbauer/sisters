a4e00299278b5233cb8adfc01f6e573134eac35b
Daniel Muckelbauer <daniel2.muckelbauer@oth-regensburg.de>
2018-10-17 23:12:40 +0200

    Refactored platform

--- Assets/Code/Scripts/BaseMovingPlatform.cs
+++ Assets/Code/Scripts/BaseMovingPlatform.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Code.Scripts
@@ -9,22 +10,14 @@ namespace Code.Scripts
         public float Pause;
         public float Step;
 
-        private Vector3 direction;
+        protected Vector3 Direction;
+        private HashSet<GameObject> childList;
 
         private void Start()
         {
-            StartCoroutine(ChangeDirection());
-            direction = Vector3.right;
-        }
-
-        private IEnumerator ChangeDirection()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(MoveTime);
-                yield return new WaitForSeconds(Pause);
-                direction = direction == Vector3.right ? Vector3.left : Vector3.right;
-            }
+            Direction = Vector3.right;
+            StartCoroutine(DirectionChangeLoop());
+            childList = new HashSet<GameObject>();
         }
 
         private void Update()
@@ -32,9 +25,35 @@ namespace Code.Scripts
             Move();
         }
 
+        private IEnumerator DirectionChangeLoop()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(MoveTime);
+                yield return new WaitForSeconds(Pause);
+                CalculateDirection();
+            }
+        }
+
+        protected abstract void CalculateDirection();
+
         private void Move()
         {
-            transform.position += direction * Step;
+            transform.position += Direction * Step * Time.deltaTime;
+        }
+
+        private void OnTriggerStay2D(Collider2D col)
+        {
+            if (childList.Contains(col.gameObject))
+                return;
+            childList.Add(col.gameObject);
+            col.gameObject.transform.parent = transform;
+        }
+
+        private void OnTriggerExit2D(Collider2D col)
+        {
+            childList.Remove(col.gameObject);
+            col.gameObject.transform.parent = null;
         }
     }
 }
--- Assets/Code/Scripts/HorizintallyMovingPlatform.cs
+++ Assets/Code/Scripts/HorizintallyMovingPlatform.cs
@@ -5,25 +5,9 @@ namespace Code.Scripts
 {
     public class HorizintallyMovingPlatform : BaseMovingPlatform
     {
-        private HashSet<GameObject> childList;
-
-        private void Awake()
+        protected override void CalculateDirection()
         {
-            childList = new HashSet<GameObject>();
-        }
-
-        private void OnTriggerStay2D(Collider2D col)
-        {
-            if (childList.Contains(col.gameObject))
-                return;
-            childList.Add(col.gameObject);
-            col.gameObject.transform.parent = transform;
-        }
-
-        private void OnTriggerExit2D(Collider2D col)
-        {
-            childList.Remove(col.gameObject);
-            col.gameObject.transform.parent = null;
+            Direction = Direction == Vector3.right ? Vector3.left : Vector3.right;
         }
     }
 }
