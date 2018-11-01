45a19bff73a26e28de30af322f2de52bf019ff79
Daniel Muckelbauer <daniel2.muckelbauer@oth-regensburg.de>
2018-11-01 18:26:37 +0100

    Added blinking to combatcontroller, not working

--- Assets/Code/Classes/CombatController/EnemyCombatController.cs
+++ Assets/Code/Classes/CombatController/EnemyCombatController.cs
@@ -1,23 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
 
 namespace Code.Classes.CombatController
 {
     public class EnemyCombatController : BaseCombatController
     {
+        private readonly List<SpriteRenderer> allRenderers;
+        private readonly Collider2D collider;
+
         public EnemyCombatController(GameObject go, int currentLife) : base(go, currentLife)
         {
+            allRenderers = GameObject.GetComponentsInChildren<SpriteRenderer>().ToList();
+            collider = GameObject.GetComponent<Collider2D>();
         }
 
         public override void ReceiveHit(Collider2D col)
         {
-            JumpUpAndDown();
+            Thread thread = new Thread(BrieflyTurnInvincibleAndBlink);
+            thread.Start();
             base.ReceiveHit(col);
         }
 
-        private void JumpUpAndDown()
+        private void BrieflyTurnInvincibleAndBlink()
         {
-            const int magnitude = 100;
-            GameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * magnitude);
+            collider.enabled = false;
+            Blink(10);
+            collider.enabled = true;
+        }
+
+        private void Blink(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                float opacity = i % 2 == 0 ? 0.5f : 1;
+                allRenderers.ForEach(r => r.color = new Color(1f, 1f, 1f, opacity));
+                Thread.Sleep(100);
+            }
         }
     }
 }
