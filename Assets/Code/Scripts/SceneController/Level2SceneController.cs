af9ed6e93666b4cf22654655ce6d890ae57eab21
Daniel Muckelbauer <daniel2.muckelbauer@oth-regensburg.de>
2018-11-06 20:23:09 +0100

    Slowly play balletmusic on boss encounter

--- Assets/Code/Scripts/SceneController/Level2SceneController.cs
+++ Assets/Code/Scripts/SceneController/Level2SceneController.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
-using Code.Classes;
+﻿using Code.Classes;
 using Code.Scripts.Entity;
+using System.Collections;
 using UnityEngine;
 
 namespace Code.Scripts.SceneController
 {
     public class Level2SceneController : BaseSceneController
     {
+        public AudioSource AudioSource;
+        public AudioClip BalletMusic;
         public Transform EndbossSpawnPoint;
 
         public override void SceneTriggerEntered()
@@ -20,11 +22,31 @@ namespace Code.Scripts.SceneController
             base.Start();
             StartCoroutine(PlayOpeningCutscene(5, 2));
         }
+
+        private void PlayBalletMusic()
+        {
+            AudioSource.clip = BalletMusic;
+            AudioSource.volume = 0f;
+            AudioSource.Play();
+            StartCoroutine(SlowlyIncreaseVolume());
+        }
+
+        private IEnumerator SlowlyIncreaseVolume()
+        {
+            while (AudioSource.volume < 0.1)
+            {
+                yield return new WaitForSeconds(2);
+                AudioSource.volume += 0.01f;
+            }
+        }
+
         private IEnumerator TalkingCutScene()
         {
             SpeechBubble munisBubble = SpeechBubbles[Character.Muni];
             SpeechBubble pollinsBubble = SpeechBubbles[Character.Pollin];
             munisBubble.ShowSpeechBubble();
+            AudioSource.Stop();
+            PlayBalletMusic();
             yield return munisBubble.ShowNextBubbleText();
             pollinsBubble.ShowSpeechBubble();
             yield return pollinsBubble.ShowNextBubbleText();
